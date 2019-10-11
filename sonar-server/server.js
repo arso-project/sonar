acb4596eb5b9da5ef567a181813d12f3a627d9fa
Franz Heinzmann (Frando) <frando@unbiskant.org>
2019-10-11 16:17:17 +0200

    Add fastify-cors, fix package.json

--- sonar-server/server.js
+++ sonar-server/server.js
@@ -1,6 +1,8 @@
 const { IslandManager } = require('@arso-project/sonar-dat')
 const Fastify = require('fastify')
 
+const fastifyCors = require('fastify-cors')
+
 const apiRoutes = require('./routes/api')
 const hyperdriveRoutes = require('./routes/hyperdrive')
 
@@ -11,6 +13,7 @@ module.exports = function createServer (opts) {
   const storagePath = opts.storage || '../.data'
   const islands = new IslandManager(storagePath)
 
+  fastify.register(fastifyCors)
   fastify.register(apiRoutes, { islands, prefix: 'api' })
   fastify.register(hyperdriveRoutes, { islands, prefix: 'fs' })
 
