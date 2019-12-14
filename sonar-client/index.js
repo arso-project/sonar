4c0a8632e8c14236a71ef1ddb5e502222bc02750
Franz Heinzmann (Frando) <frando@unbiskant.org>
2019-12-14 19:23:51 +0100

    client: default export

--- sonar-client/index.js
+++ sonar-client/index.js
@@ -1,7 +1,7 @@
 const SonarClient = require('./lib/client')
 const SearchQueryBuilder = require('./lib/searchquerybuilder')
 
-module.exports = {
+module.exports = Object.assign(SonarClient, {
   SonarClient,
   SearchQueryBuilder
-}
+})
