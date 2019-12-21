e50ca9620113bb3412fbaa334308e5fb0710343e
Franz Heinzmann (Frando) <frando@unbiskant.org>
2019-12-21 16:20:31 +0100

    ui: fix

--- sonar-ui/src/components/Error.js
+++ sonar-ui/src/components/Error.js
@@ -3,7 +3,9 @@ import errors from '../lib/error'
 
 export default function Error (props) {
   const { error } = props
-  useEffect(() => errors.push(error), [error])
+  useEffect(() => {
+    errors.push(error)
+  }, [error])
   let message
   if (typeof error === 'string') message = error
   else if (typeof error === 'object' && error.message) message = error.message
