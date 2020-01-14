7934634173028ed4c46738b129f79d4db86eef11
Franz Heinzmann (Frando) <frando@unbiskant.org>
2020-01-14 04:25:47 +0100

    ui: mobile viewport metatag

--- sonar-ui/webpack.config.js
+++ sonar-ui/webpack.config.js
@@ -62,7 +62,8 @@ const config = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Sonar'
+      title: 'Sonar',
+      meta: { viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no' }
       // template: './index.html'
     })
   ]
