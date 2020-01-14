c221b5b2b504dcd89d3360f6aea6074b1e8ffda5
Franz Heinzmann (Frando) <frando@unbiskant.org>
2020-01-14 04:25:39 +0100

    ui: move theme in folder

--- sonar-ui/src/theme/theme.js
+++ sonar-ui/src/theme/theme.js
@@ -1,12 +1,11 @@
 import { theme } from '@chakra-ui/core'
 
-console.log(theme)
-
 // Let's say you want to add custom colors
 const sonarTheme = {
   ...theme,
   colors: {
-    ...theme.colors
+    ...theme.colors,
+    main: theme.colors.cyan
   }
 }
 
