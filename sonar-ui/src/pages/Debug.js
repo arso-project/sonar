374dbe31b9338071b5aac23f9f47665a08c552e5
Franz Heinzmann (Frando) <frando@unbiskant.org>
2020-01-30 14:55:51 +0100

    ui: debug page

--- sonar-ui/src/pages/Debug.js
+++ sonar-ui/src/pages/Debug.js
@@ -1,45 +1,73 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import {
-  Box, Flex, Badge, Text, Icon, Image,
+  Box, Flex, Badge, Text, Icon, Image, PseudoBox,
   FormControl, FormLabel, Input, FormHelperText
 } from '@chakra-ui/core'
-import css from '@emotion/css'
+// import css from '@emotion/css'
 
-// Sample component from airbnb.com
-export default function DebugPage () {
+const initialState = { play: 'stop' }
+
+function reducer (state, action) {
+  const x = s => ({ ...state, ...s })
+  if (action === 'play') {
+    return x({ play: 'play' })
+  }
+  if (action === 'pause') {
+    if (state.play === 'stop') return state
+    return x({ play: 'pause' })
+  }
+  if (action === 'playpause') {
+    if (state.play === 'stop') return x({ play: 'play' })
+    if (state.play === 'pause') return x({ play: 'play' })
+    return x({ play: 'pause' })
+  }
+  if (action === 'stop') {
+    return x({ play: 'stop' })
+  }
+  if (action === 'fullscreen') {
+    return x({ fullscreen: !state.fullscreen })
+  }
+}
+
+export default function Player () {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  let style = {}
+  if (state.fullscreen) {
+    style = {
+      position: 'fixed',
+      left: 0,
+      top: 0,
+      bottom: 0,
+      right: 0,
+      zIndex: 1000
+    }
+  }
+  const size = '10px'
   return (
-    <>
-      <Box maxW='sm'>
-        <Flex align='baseline' mt={2}>
-          <Badge variantColor='pink'>Plus</Badge>
-          <Text
-            ml={2}
-            textTransform='uppercase'
-            fontSize='sm'
-            fontWeight='bold'
-            color='pink.800'
-          >
-            Foo bar
-          </Text>
-        </Flex>
-        <Text mt={2} fontSize='xl' fontWeight='semibold' lineHeight='short'>
-          Test tatoo
-        </Text>
-        <Text mt={2}>Yay</Text>
-        <Flex mt={2} align='center'>
-          <Icon name='star' color='orange.400' />
-          <Text ml={1} fontsize='sm'><b>4.84</b> (190)</Text>
-        </Flex>
-      </Box>
-      <Box>
-        <FormControl maxW='md'>
-          <FormLabel htmlFor='email'>Pubkey</FormLabel>
-          <Input type='email' id='email' aria-describedby='email-helper-text' />
-          <FormHelperText id='email-helper-text' css={css`color: red`}>
-            Invalid key
-          </FormHelperText>
-        </FormControl>
-      </Box>
-    </>
+    <Flex direction='column' bg='black' w='100%' textAlign='center' contentAlign='center' style={style}>
+      <Flex flex={1}>
+        <Text fontSize='xl' color='green.400' p='10px'>{state.play}</Text>
+      </Flex>
+      <Flex flexBasis='100px' m={size}>
+        <PButton onClick={e => dispatch('play')} active={state.play === 'play'}>Play</PButton>
+        <PButton onClick={e => dispatch('pause')} active={state.play === 'pause'}>Pause</PButton>
+        <PButton onClick={e => dispatch('playpause')} active={state.play === 'play'}>Playpause</PButton>
+        <PButton onClick={e => dispatch('stop')} active={state.play === 'stop'}>Stop</PButton>
+        <Box flex={1} />
+        <PButton onClick={e => dispatch('fullscreen')} active={state.fullscreen}>Fullscreen</PButton>
+      </Flex>
+    </Flex>
+  )
+}
+
+function PButton (props) {
+  const { children, active, ...other } = props
+  const size = '100px'
+  other.bg = active ? 'green.400' : 'red.400'
+  other._hover = { boxShadow: '0 0 10px 2px rgba(255, 50, 200, 0.7)' }
+  return (
+    <PseudoBox as='button' w={size} h={size} mr='10px' {...other}>
+      {children}
+    </PseudoBox>
   )
 }
