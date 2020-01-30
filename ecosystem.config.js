5197607992f5448fb96dffe1a94c04044cc7e2ac
Franz Heinzmann (Frando) <frando@unbiskant.org>
2020-01-30 20:48:12 +0100

    add ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,38 @@
+// Start with `pm2 start ecosystem.config.js`
+module.exports = {
+  apps: [
+    {
+      name: 'server',
+      script: './sonar-server/bin.js',
+      args: 'server start',
+      instances: 1,
+      // autorestart: true,
+      // max_restarts: 1,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+        DEBUG: '*'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    },
+    {
+      name: 'ui',
+      script: './sonar-ui/bin.js',
+      args: 'ui serve',
+      instances: 1,
+      // autorestart: true,
+      // max_restarts: 1,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ]
+}
