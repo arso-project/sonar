94694f501bd3385fe4bdc57c9a6020f2cdccbf63
Franz Heinzmann (Frando) <frando@unbiskant.org>
2020-03-22 15:02:54 +0100

    ui: own server for dev builds
    * removes webpack-plugin-serve and replaces it with an express
      server plus webpack-dev-middleware in our custom binary.
      this makes it easier to keep in sync with other server code.
    * introduces a workdir option to use the webpack config for builds
      of other ui modules
    * use babel.config.json so that other babelrcs can overlay
    * remove old code from webpack config

--- sonar-ui/bin.js
+++ sonar-ui/bin.js
@@ -3,13 +3,39 @@
 const { spawn } = require('child_process')
 const express = require('express')
 const p = require('path')
+const fs = require('fs')
 const open = require('open')
 const yargs = require('yargs')
 
 // TODO: This pulls webpack-nano into packaging.
 // Should be optional.
-const WP_BIN = require.resolve('webpack-nano/bin/wp.js')
-const WP_CONFIG = p.join(__dirname, 'webpack.config.js')
+const WP_BIN = process.env.WP_BIN || require.resolve('webpack-nano/bin/wp.js')
+const DIST_PATH = p.join('build', 'dist')
+
+const commonOptions = {
+  workdir: {
+    alias: 'd',
+    describe: 'workdir where source files are',
+    default: process.env.WORKDIR || __dirname
+  }
+}
+const serveOptions = {
+  port: {
+    alias: 'p',
+    describe: 'port to listen on for HTTP request',
+    default: process.env.PORT || 55555
+  },
+  hostname: {
+    alias: 'h',
+    describe: 'hostname to listen on for HTTP request',
+    default: process.env.HOSTNAME || 'localhost'
+  },
+  open: {
+    alias: 'o',
+    type: 'boolean',
+    describe: 'open UI in browser'
+  }
+}
 
 const command = {
   command: 'ui',
@@ -21,13 +47,15 @@ const command = {
       .command({
         command: 'dev',
         describe: 'Start Sonar UI in dev mode (rebuilds on changes)',
-        handler: dev
+        handler: dev,
+        builder: { ...commonOptions, ...serveOptions }
       })
       .command({
         command: 'build',
         describe: 'Build the ui',
         handler: build,
         builder: {
+          ...commonOptions,
           static: {
             boolean: true,
             describe: 'Build a static HTML export'
@@ -37,33 +65,12 @@ const command = {
       .command({
         command: ['start', 'serve'],
         describe: 'Serve Sonar UI over HTTP',
-        builder: {
-          port: {
-            alias: 'p',
-            describe: 'port to listen on for HTTP request',
-            default: 55555
-          },
-          hostname: {
-            alias: 'h',
-            describe: 'hostname to listen on for HTTP request',
-            default: 'localhost'
-          },
-          open: {
-            alias: 'o',
-            type: 'boolean',
-            describe: 'open UI in browser'
-          }
-          // 'insecure-api-proxy': {
-          //   type: 'boolean',
-          //   describe: 'serve a proxy to the sonar api'
-          // }
-        },
+        builder: { ...commonOptions, ...serveOptions },
         handler: serve
       })
   }
 }
 
-// const args = cli.command(command)
 if (require.main === module) {
   command.builder(yargs)
   yargs.demandCommand().help().parse()
@@ -72,69 +79,109 @@ if (require.main === module) {
   module.exports.serve = serve
 }
 
-function dev (argv) {
-  console.log('Starting UI in dev mode')
-  console.log('Webpack config: ' + WP_CONFIG)
-  const cmd = spawn('node', [WP_BIN, '--config', WP_CONFIG, '--serve'], {
-    stdio: 'inherit',
-    cwd: __dirname,
-    env: process.env
-  })
-  // cmd.stdout.pipe(process.stdout)
-  // cmd.stderr.pipe(process.stderr)
+function createServer (opts) {
+  const app = express()
+  const { port, hostname } = opts
+  app.start = function () {
+    app.listen(port, hostname, (err) => {
+      if (err) return console.error(err)
+      const link = `http://${hostname}:${port}`
+      console.log(`UI ready on ${link}`)
+      if (opts.open) open(link)
+    })
+  }
+  return app
 }
 
-function build (argv) {
-  console.log('Building UI')
-  console.log('Webpack config: ' + WP_CONFIG)
-  const args = []
-  if (argv.static) args.push('--static')
-  if (argv.watch) args.push('--watch')
-  const cmd = spawn('node', [WP_BIN, '--config', WP_CONFIG, args], {
-    stdio: 'inherit',
-    cwd: __dirname,
-    env: process.env
-  })
-  // cmd.stdout.pipe(process.stdout)
-  // cmd.stderr.pipe(process.stderr)
+function dev (argv) {
+  console.log('Starting UI in dev mode')
+  argv.workdir = p.resolve(argv.workdir)
+  const configPath = findWebpackConfig(argv.workdir)
+  console.log('Webpack config: ' + configPath)
+
+  process.env.NODE_ENV = 'development'
+  process.env.WORKDIR = argv.workdir
+
+  const app = createServer(argv)
+
+  const config = require(configPath)
+  const webpack = require('webpack')
+
+  // Enable hot module replacement.
+  config.plugins.push(new webpack.HotModuleReplacementPlugin())
+  config.entry.unshift(require.resolve('webpack-hot-middleware/client'))
+
+  const compiler = webpack(config)
+  app.use(
+    require('webpack-dev-middleware')(compiler, {
+      publicPath: config.output.publicPath,
+      stats: 'minimal'
+      // TODO: Somehow the 'preset' setting isn't working
+      // stats: {
+      //   preset: 'minimal',
+      //   moduleTrace: true,
+      //   errorDetails: true
+      // }
+    })
+  )
+  app.use(
+    require('webpack-hot-middleware')(compiler, {
+      log: false,
+      path: '/__webpack_hmr',
+      heartbeat: 10 * 1000
+    })
+  )
+
+  app.start()
 }
 
 function serve (argv) {
-  const app = express()
-  const path = p.join(__dirname, 'dist')
-  app.use(express.static(path))
+  const app = createServer(argv)
+  const staticPath = p.join(p.resolve(argv.workdir), DIST_PATH)
+  app.use(express.static(staticPath))
+  app.start()
+}
 
-  const { port, hostname } = argv
-  app.listen(port, hostname, (err) => {
-    if (err) return console.error(err)
-    const link = `http://${hostname}:${port}`
-    console.log(`UI ready on ${link}`)
-    if (argv.open) open(link)
+function build (argv) {
+  console.log('Start building UI')
+  argv.workdir = p.resolve(argv.workdir)
+  const configPath = findWebpackConfig(argv.workdir)
+  console.log('Webpack config: ' + configPath)
+
+  process.env.NODE_ENV = 'production'
+  process.env.WORKDIR = argv.workdir
+
+  const args = copyArgs(argv, ['static', 'watch', 'port', 'hostname', 'workdir'])
+  spawn('node', [WP_BIN, '--config', configPath, ...args], {
+    stdio: 'inherit',
+    cwd: __dirname,
+    env: process.env
   })
+}
 
-  // const httpProxy = require('http-proxy')
-  // const crypto = require('crypto')
-  // const TOKEN_HEADER = 'x-sonar-access-token'
-  // const token = crypto.randomBytes(16).toString('hex')
-  // const endpoint = argv.endpoint || 'http://localhost:9191/api'
-  // if (argv.insecureApiProxy) {
-  //   // To be able to actually serve a useful remote UI a proxy
-  //   // onto the API server is created.
-  //   // TODO: Rethink this of course!
-  //   // Likely it should work the other way round:
-  //   // - The server opens a proxy for the API (or just serves it)
-  //   // - The token is checked there
-  //   // And then use proper ephemeral tokens or hashes.
-  //   const proxy = httpProxy.createServer()
-  //   app.use('/api', (req, res, next) => {
-  //     // A very (too) simple auth scheme: if you have the single
-  //     // token per running session, you're good. otherwise not.
-  //     // The UI can remember the token for you.
-  //     if (req.headers[TOKEN_HEADER] !== token) {
-  //       return res.status(401).send({ error: 'invalid access token' })
-  //     }
-  //     proxy.web(req, res, { target: endpoint })
-  //   })
-  //   console.log(`Link with API access:\n${link}/#/token/${token}`)
-  // }
+function findWebpackConfig (workdir) {
+  if (fs.existsSync(p.join(workdir, 'webpack.config.js'))) {
+    return p.join(workdir, 'webpack.config.js')
+  } else {
+    return p.join(__dirname, 'webpack.config.js')
+  }
+}
+
+function copyArgs (from, keys) {
+  const args = []
+  for (const key in keys) {
+    if (from[key] !== undefined) {
+      args.push(argkey(key))
+      if (from[key] !== true) {
+        args.push(from[key])
+      }
+    }
+  }
+  return args
+  function argkey (key) {
+    return '--' + key.replace(
+      /[\w]([A-Z])/g,
+      m => m[0] + '-' + m[1]
+    ).toLowerCase()
+  }
 }
--- sonar-ui/webpack.config.js
+++ sonar-ui/webpack.config.js
@@ -1,120 +1,104 @@
 const argv = require('webpack-nano/argv')
 const p = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { WebpackPluginServe } = require('webpack-plugin-serve')
 const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
 
-const isDev = argv.watch || argv.serve || process.env.NODE_ENV === 'development'
-let output = 'dist'
-let ramdisk = false
-if (isDev) {
-  // Optional ramdisk arg to build in a ramdisk (faster!)
-  ramdisk = !!argv.ramdisk || !!process.env.WP_RAM
-  output = ramdisk ? 'build-ramdisk' : 'build'
+const opts = {
+  // A workdir is the main entrypoint. Entry file is expected at src/index.js.
+  workdir: p.resolve(argv.workdir || process.env.WORKDIR || __dirname),
+  // Enable dev mode (source maps, hot reloading)
+  dev: process.env.NODE_ENV === 'development',
+  // Build in stati mode (single html file)
+  static: argv.static || process.env.WEBPACK_STATIC,
+  // Analyze bundle size
+  analzye: argv.analyze || process.env.WEBPACK_ANALYZE,
+  // Benchmark build time
+  bench: argv.bench || process.env.WEBPACK_BENCH
 }
-output = p.join(__dirname, output)
 
-let config = {
-  entry: ['./src/index.js'],
-  mode: isDev ? 'development' : 'production',
-  watch: argv.watch || argv.serve,
-  devtool: isDev ? 'eval-source-map' : 'none',
-  stats: 'minimal',
-  module: {
-    rules: [
-      // Transpile JS and JSX with babel
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              // plugins: [isDev && require.resolve('react-refresh/babel')].filter(Boolean)
-            }
-          }
-        ]
-      }
-      // Support importing CSS files and process them with PostCSS
-      // {
-      //   test: /\.(css|pcss)$/,
-      //   use: [
-      //     'style-loader',
-      //     { loader: 'css-loader', options: { importLoaders: 1 } },
-      //     'postcss-loader'
-      //   ]
-      // },
-      // Support importing font files (also from CSS files)
-      // {
-      //   test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-      //   use: [
-      //     {
-      //       loader: 'file-loader',
-      //       options: {
-      //         name: '[name].[ext]',
-      //         outputPath: 'fonts/'
-      //       }
-      //     }
-      //   ]
-      // }
-    ]
-  },
-  output: {
-    path: output,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  plugins: [
-    // Create an index.html file
-    new HtmlWebpackPlugin({
-      title: 'Sonar',
-      meta: { viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no' },
-      inlineSource: '.(js|css)$' // has only an effect with HtmlWebpackInlineSourcePlugin
-      // template: './index.html'
-    })
+module.exports = createConfig(opts)
+
+function createConfig (opts) {
+  const target = opts.dev ? 'debug' : 'dist'
+  const output = p.join(opts.workdir, 'build', target)
+
+  const entry = [
+    p.join(opts.workdir, './src/index.js')
   ]
-}
 
-// --static: Include all CSS and JS directly in a singel HTML file
-if (argv.static) {
-  console.log('Building in static mode')
-  config.plugins.push(new HtmlWebpackInlineSourcePlugin())
-}
+  let config = {
+    entry,
+    mode: opts.dev ? 'development' : 'production',
+    watch: argv.watch || argv.serve,
+    devtool: opts.dev ? 'inline-source-map' : 'none',
+    stats: 'minimal',
+    module: {
+      rules: [
+        // Transpile JS and JSX with babel
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: require.resolve('babel-loader'),
+              options: {
+                // This is our base .babelrc. Using a config file allows us
+                // to reuse it in different workdirs.
+                configFile: p.join(__dirname, 'babel.config.json'),
+                // Search for babelrc in the workdir, and allow here too.
+                babelrcRoots: [
+                  __dirname,
+                  opts.workdir
+                ],
+                // Include react-hot-loader in dev mode.
+                plugins: opts.dev ? [require.resolve('react-hot-loader/babel')] : undefined
+              }
+            }
+          ]
+        }
+      ]
+    },
+    output: {
+      path: output,
+      publicPath: '/',
+      filename: 'bundle.js',
+      pathinfo: false,
+    },
+    plugins: [
+      // Create an index.html file
+      new HtmlWebpackPlugin({
+        title: 'Sonar',
+        meta: { viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no' },
+        inlineSource: '.(js|css)$' // has only an effect with HtmlWebpackInlineSourcePlugin
+      })
+    ]
+  }
 
-// --analyze: Analyze bundle size
-if (argv.analyze || process.env.WEBPACK_ANALYZE) {
-  const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
-  config.plugins.push(new BundleAnalyzerPlugin())
-}
+  // --static: Include all CSS and JS directly in a singel HTML file
+  if (opts.static) {
+    console.log('Building in static mode')
+    config.plugins.push(new HtmlWebpackInlineSourcePlugin())
+  }
 
-// --serve: Spin up a web server to serve the built UI
-if (argv.serve) {
-  config.plugins.push(
-    new WebpackPluginServe({
-      host: 'localhost',
-      static: output,
-      open: false,
-      // liveReload: true,
-      hmr: true,
-      historyFallback: true,
-      // progress: 'minimal',
-      progress: false,
-      ramdisk: ramdisk
+  // --analyze: Analyze bundle size
+  if (opts.analyze) {
+    const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+    config.plugins.push(new BundleAnalyzerPlugin())
+  }
+
+  // Include react-hot-loader patch in dev mode
+  if (opts.dev) {
+    config.entry.unshift(require.resolve('react-hot-loader/patch'))
+  }
+
+  // --bench: Measure webpack build time
+  if (opts.bench) {
+    const SpeedMeasurePlugin = require('speed-measure-webpack-plugin')
+    const smp = new SpeedMeasurePlugin({
+      // granularLoaderData: true
     })
-  )
-  // config.plugins.push(new ReactRefreshWebpackPlugin())
-  config.entry.push(
-    'webpack-plugin-serve/client'
-  )
-}
+    config = smp.wrap(config)
+  }
 
-// --bench: Measure webpack build time
-if (argv.bench || process.env.WEBPACK_BENCH) {
-  const SpeedMeasurePlugin = require('speed-measure-webpack-plugin')
-  const smp = new SpeedMeasurePlugin({
-    // granularLoaderData: true
-  })
-  config = smp.wrap(config)
+  return config
 }
-
-module.exports = config
