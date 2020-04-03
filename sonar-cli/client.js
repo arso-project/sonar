73e6dbef21414b9e8cb20ff67c002f7846e5bb70
Franz Heinzmann (Frando) <frando@unbiskant.org>
2020-04-03 21:42:55 +0200

    cli: client opts

--- sonar-cli/client.js
+++ sonar-cli/client.js
@@ -1,6 +1,10 @@
 const Client = require('@arso-project/sonar-client')
 
 module.exports = function getClient (argv) {
-  const client = new Client(argv.endpoint, argv.island)
+  const client = new Client({
+    endpoint: argv.endpoint,
+    island: argv.island,
+    cache: false
+  })
   return client
 }
