e4a79af3195d23d9110c2f7f464668f81c1a740e
Franz Heinzmann (Frando) <frando@unbiskant.org>
2020-04-10 13:29:20 +0200

    improve networking code

--- sonar-core/lib/network.js
+++ sonar-core/lib/network.js
@@ -56,65 +56,72 @@ module.exports = class Network {
   }
 
   remove (island) {
-    // TODO.
+    island.ready(() => {
+      const hdkey = island.discoveryKey.toString('hex')
+      if (!this.replicating[hdkey]) return
+      for (const peer of this.peers[hdkey]) {
+        peer.stream.destroy()
+      }
+      this.replicating[hdkey] = undefined
+    })
   }
 
   close (cb = noop) {
-    Object.values(this.peers).forEach(peer => peer.stream && peer.stream.destroy())
+    this._closing = true
+    Object.values(this.peers).forEach(peer => {
+      peer.socket && peer.socket.destroy()
+    })
     // TODO: the destroy method of hyperswarm can also take a callback.
     // Awaiting this callback takes a long time. Investigate why this
     // is the case and what's it needed for. For now, the callback is
     // not awaited because of how long it takes and because things seem
     // to work this way too.
-    this.hyperswarm.destroy()
-    this.localswarm.close(cb)
+    process.nextTick(() => {
+      this.hyperswarm.destroy()
+      this.localswarm.close(() => {
+        this._closing = false
+        this.opened = false
+        cb()
+      })
+    })
   }
 
-  _onpeer ({ stream, discoveryKey }) {
-    stream.on('error', err => debug('error', err))
-    this.peers[discoveryKey] = this.peers[discoveryKey] || []
-    this.peers[discoveryKey].push(stream)
+  _onpeer (dkey, protocol) {
+    const hdkey = dkey.toString('hex')
+    const island = this.replicating[hdkey]
+    if (!island) {
+      debug('invalid discovery key')
+      protocol.destroy()
+    } else {
+      debug('start replication [init: false, discoveryKey: %s]', hdkey)
+      island.replicate(false, { stream: protocol, live: true })
+      this.peers[hdkey] = this.peers[hdkey] || []
+      this.peers[hdkey].push({ protocol, discoveryKey: dkey })
+    }
+    protocol.on('error', err => debug('protocol error', err))
   }
 
   _onconnection (socket, details) {
+    socket.on('error', err => {
+      if (!this._closing) debug('socket error', err)
+    })
+
     const isInitiator = !!details.client
-    // const proto = new Protocol(isInitiator)
-    var stream = null
     const dkey = details.peer ? details.peer.topic : null
+
     debug('onconnection', isInitiator, dkey ? dkey.toString('hex') : null)
     if (isInitiator) {
-      const hdkey = dkey.toString('hex')
-      const island = this.replicating[hdkey]
-      if (!island) {
-        debug('invalid discovery key')
-        socket.destroy()
-        return
-      }
-      debug('start replication [init: true]!')
-      stream = island.replicate(true, { live: true })
-      this._onpeer({ stream, discoveryKey: hdkey, island })
+      const protocol = new Protocol(true)
+      this._onpeer(dkey, protocol)
+      debug('start protocol [init: true]')
     } else {
-      stream = new Protocol(false)
-      stream.once('discovery-key', dkey => {
+      const protocol = new Protocol(false)
+      debug('start protocol [init: false]')
+      protocol.once('discovery-key', dkey => {
         debug('ondiscoverykey', dkey.toString('hex'))
-        const hdkey = dkey.toString('hex')
-        const island = this.replicating[hdkey]
-        if (!island) {
-          debug('invalid discovery key')
-        } else {
-          debug('start replication [init: false]')
-          island.replicate(false, { stream, live: true })
-          this._onpeer({ stream, discoveryKey: hdkey, island })
-        }
+        this._onpeer(dkey, protocol)
       })
     }
-    pump(stream, socket, stream)
-    socket.on('error', err => {
-      debug('socket error', err)
-    })
-    stream.on('error', err => {
-      debug('proto error', err)
-    })
   }
 }
 
