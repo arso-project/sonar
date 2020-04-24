127a26e6dd9fd87a3da9ad7efaffbf156a6aec30
Franz Heinzmann (Frando) <frando@unbiskant.org>
2020-04-24 12:23:08 +0200

    ui: enable emotion css prop

--- sonar-ui/src/pages/Start.js
+++ sonar-ui/src/pages/Start.js
@@ -13,7 +13,7 @@ export default function StartPage (props) {
 
   return (
     <Box flex='1'>
-      <Text color='teal.400' fontSize={['lg', '2xl', '4xl']} my='8' textAlign='center'>
+      <Text color='teal.400' fontSize={['lg', '2xl', '5xl']} my='8' textAlign='center'>
         Hello friend, welcome to Sonar!
       </Text>
     </Box>
