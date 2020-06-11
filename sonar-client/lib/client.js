f15e2387d1f3379e83b28dc62d50a7cde6ca8567
Franz Heinzmann (Frando) <frando@unbiskant.org>
2020-06-11 23:50:33 +0200

    client: remove axios

--- sonar-client/lib/client.js
+++ sonar-client/lib/client.js
@@ -1,6 +1,5 @@
 const debug = require('debug')('sonar-client')
 const randombytes = require('randombytes')
-const axios = require('axios')
 const fetch = require('isomorphic-fetch')
 
 const Commands = require('./commands')
@@ -121,90 +120,10 @@ module.exports = class Client {
       throw err
     }
   }
-
-  async fetchAxios (path, opts = {}) {
-    if (Array.isArray(path)) path = path.join('/')
-    if (!path.startsWith('/')) path = '/' + path
-    const url = this.endpoint + path
-    const request = {
-      method: opts.method,
-      url,
-      maxRedirects: 0,
-      headers: {
-        'content-type': 'application/json',
-        ...opts.headers || {}
-      },
-      // axios has a very weird bug that it REMOVES the
-      // Content-Type header if data is empty...
-      data: opts.body || {},
-      params: opts.params,
-      onUploadProgress: opts.onUploadProgress
-    }
-    debug('request: %s %s', request.method, request.url)
-
-    if (opts.binary) {
-      request.headers['content-type'] = 'application/octet-stream'
-    }
-    if (opts.stream) {
-      request.responseType = 'stream'
-    }
-
-    try {
-      debug('request: %s %s params %o', request.method, request.url, request.params)
-      const result = await axios.request(request)
-      debug('response: %s %s (length %s)', result.status, result.statusText, result.headers['content-length'])
-      return result.data
-    } catch (err) {
-      const wrappedError = wrapAxiosError(err)
-      debug('response: %o', wrappedError.message)
-      throw wrappedError
-    }
-  }
 }
 
 function isJsonResponse (res) {
   const header = res.headers.get('content-type')
   if (!header) return false
   return header.indexOf('application/json') !== -1
-}
-
-function wrapAxiosError (err) {
-  const log = {}
-  const { request, response, config } = err
-  if (config) {
-    log.config = {
-      url: config.url,
-      method: config.method,
-      headers: config.headers,
-      data: config.data
-    }
-    err.config = log.config
-  }
-  if (request) {
-    log.request = {
-      method: request.method,
-      path: request.path
-    }
-    err.request = log.request
-  }
-  if (response) {
-    log.response = {
-      status: response.status,
-      statusText: response.statusText,
-      headers: response.headers,
-      data: response.data
-    }
-    err.response = log.response
-  }
-  // debug(log)
-
-  let msg
-  if (err && err.response && typeof err.response.data === 'object' && err.response.data.error) {
-    msg = err.response.data.error
-  } else {
-    msg = err.message
-  }
-  err.remoteError = msg
-  if (msg) err.message = err.message + ` (reason: ${msg})`
-  return err
 }
