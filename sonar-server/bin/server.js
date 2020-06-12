b25eec11f28bc80de91773d5c7831b9c331fc36f
Franz Heinzmann (Frando) <frando@unbiskant.org>
2020-06-12 23:36:25 +0200

    server: cleaner exit on error

--- sonar-server/bin/server.js
+++ sonar-server/bin/server.js
@@ -54,7 +54,7 @@ function startServer (argv) {
     process.exit(code)
   })
 
-  onexit((cb) => {
+  onexit((cb = noop) => {
     closing = true
     if (proc.killed || proc.exitCode !== null) return cb()
     proc.once('exit', cb)
