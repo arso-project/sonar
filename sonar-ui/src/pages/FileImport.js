1cfc785e45d1e841e1793dd474fb80caefe278e2
Franz Heinzmann (Frando) <frando@unbiskant.org>
2020-06-19 18:34:19 +0200

    ui: remove filebrowser special case

--- sonar-ui/src/pages/FileImport.js
+++ sonar-ui/src/pages/FileImport.js
@@ -1,16 +1,10 @@
 import React from 'react'
-import config from '../lib/config'
 import FileImporter from '../components/FileImporter'
 
-import { Redirect } from 'react-router-dom'
 
 import { Box } from '@chakra-ui/core'
 
 export default function FileImportPage (props) {
-  if (!config.get('endpoint')) {
-    return <Redirect to='/config' />
-  }
-
   return (
     <Box flex='1'>
       <FileImporter />
