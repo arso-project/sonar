32dcda16da302a44891804a6b468d64bcb3060f5
Franz Heinzmann (Frando) <frando@unbiskant.org>
2020-12-30 03:31:12 +0100

    bots: exports

---
+++ packages/bots/index.js
@@ -0,0 +1,10 @@
+const { run, runSync, readYaml } = require('./run')
+const { BotSession, CollectionSession } = require('./lib/bot')
+
+module.exports = {
+  run,
+  runSync,
+  readYaml,
+  BotSession,
+  CollectionSession
+}
