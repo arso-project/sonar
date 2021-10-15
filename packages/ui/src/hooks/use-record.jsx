95b16abfbc26ccfa8aac152daeed77b2f1e8f522
Franz Heinzmann (Frando) <frando@unbiskant.org>
2021-10-15 14:31:11 +0200

    ui: remove obsolete files

--- packages/ui/src/hooks/use-record.jsx
+++
@@ -1,9 +0,0 @@
-import { useCollection, useRecord } from '@arsonar/react'
-
-export default function useRecords (id) {
-  const record = useRecord({ id })
-  const collection = useCollection()
-  const types = collection.schema.getTypes()
-  if (!record) return null
-  return { types, record }
-}
