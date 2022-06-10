e8345fd1075bc692a85452aea21701bdb6ff1941
Franz Heinzmann (Frando) <frando@unbiskant.org>
2022-06-10 12:31:19 +0200

    common: simplify record data structure and cleanups

--- packages/common/lib/emitter.ts
+++ packages/common/lib/emitter.ts
@@ -1,25 +1,25 @@
-const kSubs = Symbol('kSubs')
-const kSelf = Symbol('kSelf')
 
 export type EmitCb<T> = (obj: T) => void
 
 export class Emitter<T> {
-  [kSubs]: Set<EmitCb<T>> = new Set();
-  [kSelf]: T
+  private subscribers: Set<EmitCb<T>> = new Set();
+  private value: T
 
-  constructor (self: T) {
-    this[kSelf] = self
+  constructor (value: T) {
+    this.value = value
   }
 
   subscribe (fn: EmitCb<T>, triggerNow = false) {
-    this[kSubs].add(fn)
-    if (triggerNow) { fn(this[kSelf]) }
-    return () => this[kSubs].delete(fn)
+    this.subscribers.add(fn)
+    if (triggerNow) {
+      fn(this.value)
+    }
+    return () => this.subscribers.delete(fn)
   }
 
   emit () {
-    for (const fn of this[kSubs]) {
-      fn(this[kSelf])
+    for (const fn of this.subscribers) {
+      fn(this.value)
     }
   }
 }
--- packages/common/lib/record/record.ts
+++ packages/common/lib/record/record.ts
@@ -1,57 +1,117 @@
 import { SchemaMember } from '../base.js'
 import { Schema } from '../schema.js'
-import { RecordVersion, WireRecordVersion, RecordValue, FieldValue } from './version.js'
-import Versions from '../versions.js'
-import { Emitter, EmitCb } from '../emitter'
-import { SC } from '../symbols'
+import { EmitCb } from '../emitter.js'
+import { SC } from '../symbols.js'
 // @ts-ignore
 import { inspectSymbol, InspectOptions } from '../util/inspect'
 
+import { RecordVersion, WireRecordVersion, RecordValue, FieldValue } from './version.js'
+
 export class Record extends SchemaMember {
-  private _versions: Versions
-  private _emitter: Emitter<Record>
   private _id?: string
   private _type?: string
+  private _versions: Map<string, RecordVersion> = new Map()
+  private _current: Set<RecordVersion> = new Set()
+  private _outdated: Set<string> = new Set()
+  private subscribers: Set<EmitCb<Record>> = new Set()
+
   constructor (schema: Schema, initialVersion: RecordVersion) {
     super(schema)
-    this._emitter = new Emitter(this as Record)
-    this._versions = new Versions()
     this.addVersion(initialVersion)
-    this._versions.subscribe(() => this.emit())
   }
 
-  emit () {
-    this._emitter.emit()
+
+  /** Subscribe to changes to this record.
+   */
+  subscribe (fn: EmitCb<Record>, triggerNow = false): () => void {
+    this.subscribers.add(fn)
+    if (triggerNow) {
+      fn(this)
+    }
+    return () => { this.subscribers.delete(fn) }
   }
 
-  subscribe (fn: EmitCb<Record>) {
-    this._emitter.subscribe(fn)
+  private _emit () {
+    for (const fn of this.subscribers) {
+      fn(this)
+    }
   }
 
-  versions () {
-    return this._versions.current()
+  get length () {
+    return this._versions.size
   }
 
-  allVersions () {
-    return this._versions.all()
+  get hasConflict () {
+    return this._current.size > 1
   }
 
-  addVersion (recordVersion: RecordVersion | WireRecordVersion) {
-    if (!(recordVersion instanceof RecordVersion)) {
-      recordVersion = new RecordVersion(this[SC], recordVersion)
+  currentVersions (): RecordVersion[] {
+    return Array.from([...this._current.values()])
+  }
+
+  allVersions (): RecordVersion[] {
+    return Array.from([...this._versions.values()])
+  }
+
+  _latest (): RecordVersion {
+    return this.currentVersions().reduce((latest, current) => {
+      if (!latest) { return current }
+      if (current.timestamp > latest.timestamp) { return current }
+      return latest
+    })
+  }
+
+  hasVersion (version: RecordVersion | string) {
+    let versionAddress
+    if (version instanceof RecordVersion) {
+      versionAddress = version.address
+    } else {
+      versionAddress = version
+    }
+    if (this._versions.has(versionAddress)) return true
+    return false
+  }
+
+  addVersion (version: RecordVersion | WireRecordVersion) {
+    if (!(version instanceof RecordVersion)) {
+      version = new RecordVersion(this[SC], version)
     }
     if (!this._id) {
-      this._id = recordVersion.id
-      this._type = recordVersion.type
+      this._id = version.id
+      this._type = version.type
     }
-    if (recordVersion.path !== this.path) {
+
+    if (this._versions.has(version.address)) return
+
+    if (version.path !== this.path) {
       throw new Error('RecordVersion does not match Record path')
     }
-    this._versions.put(recordVersion)
-  }
 
-  hasVersion (address: string) {
-    this._versions.has(address)
+    this._versions.set(version.address, version)
+
+    let isCurrent = true
+    if (!version.links) { version.inner.links = [] }
+    if (this._outdated.has(version.address)) {
+      isCurrent = false
+    } else {
+      for (const current of this._current) {
+        if (current.links?.indexOf(version.address) !== -1) {
+          isCurrent = false
+        }
+      }
+    }
+    for (const link of version.links) {
+      this._outdated.add(link)
+    }
+    if (isCurrent) {
+      for (const link of version.links) {
+        if (this._versions.has(link)) {
+          this._current.delete(this._versions.get(link)!)
+        }
+      }
+      this._current.add(version)
+    }
+    this._emit()
   }
 
   get path () {
@@ -59,7 +119,7 @@ export class Record extends SchemaMember {
   }
 
   get latest (): RecordVersion {
-    return this._versions.latest()
+    return this._latest()
   }
 
   get id () {
@@ -163,7 +223,7 @@ export class Record extends SchemaMember {
     const s = (str: string) => stylize(str, 'string')
     return `${ind}Record(
 ${ind}  ${s('type')} ${this.type} ${s('id')} ${this.id}
-${ind}  ${s('versions')} ${this._versions.length} ${s('conflict?')} ${String(this._versions.conflict)}
+${ind}  ${s('versions')} ${this.length} ${s('conflict?')} ${String(this.hasConflict)}
 ${ind}  ${s('latest')} ${this.latest.inspect(0, { indentationLvl: ind.length + 2 })}
 ${ind})`
   }
