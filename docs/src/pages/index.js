f19c43c4f490a768ef594aad83100a0d5afa8414
Franz Heinzmann (Frando) <frando@unbiskant.org>
2022-06-23 15:08:00 +0200

    docs: include logo on front page

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useColorMode } from '@docusaurus/theme-common';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
@@ -9,10 +10,14 @@ import styles from './index.module.css';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
+  const { colorMode } = useColorMode()
+  const src = colorMode === 'dark' ? 'sonar.dark.png' : 'sonar.png'
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
+        <h1 className="hero__title">
+          <img src={'/img/' + src} alt={siteConfig.title} title={siteConfig.title} />
+        </h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
