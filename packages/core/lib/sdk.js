ae2dda2ccee1d0ac393772db206ac935a909d0f3
Franz Heinzmann (Frando) <frando@unbiskant.org>
2022-10-28 12:22:26 +0200

    ensure swarm is created by hyper-sdk

--- packages/core/lib/sdk.js
+++ packages/core/lib/sdk.js
@@ -1,5 +1,6 @@
 module.exports = async function createSDK (opts = {}) {
   opts.autoJoin = false
+  delete opts.swarm
   const { create } = await import('hyper-sdk')
   const sdk = await create(opts)
   return sdk
